41ca07b314203cd96ef871d12e2a3157c53bca4e
cyprientemateu <cyprien.temateu@yahoo.com>
2024-12-15 14:50:13 -0600

    new files added

--- terraform-cleanup.sh
+++ terraform-cleanup.sh
@@ -7,13 +7,15 @@ find . -type f -name '.terraform.lock.hcl' -exec rm -f {} +
 find . -type f -name 'terraform.tfstate' -exec rm -f {} +
 find . -type f -name 'terraform.tfstate.backup' -exec rm -f {} +
 find . -type f -name '.terraform.tfstate.lock.info' -exec rm -f {} +
-# find . -type f -name 'terraform.tfstate' -exec rm -f {} +
-# find . -type f -name 'terraform.tfstate.backup' -exec rm -f {} +
-# find "/home/cypri/OneDrive/Desktop" -type f -name 'terraform.tfstate' -exec rm -f {} \;
-# find "/home/cypri/OneDrive/Desktop" -type f -name 'terraform.tfstate.backup' -exec rm -f {} \;
-# Delete terragrunt directories and files
 find . -type d -name '.terragrunt' -exec rm -rf {} +
 find . -type d -name '.terragrunt-cache' -exec rm -rf {} +
-# find "/home/cypri/OneDrive/Desktop" -type d -name '.terragrunt' -exec rm -rf {} \;
-# find "/home/cypri/OneDrive/Desktop" -type d -name '.terragrunt-cache' -exec rm -rf {} \;
+
+# find "/home/cypri/OneDrive/Desktop" -type f -name '.terraform' -exec rm -f {} +
+# find "/home/cypri/OneDrive/Desktop" -type f -name 'terraform.tfstate' -exec rm -f {} +
+# find "/home/cypri/OneDrive/Desktop" -type f -name 'terraform.tfstate.backup' -exec rm -f {} +
+# find "/home/cypri/OneDrive/Desktop" -type f -name '.terraform.lock.hcl' -exec rm -f {} +
+# find "/home/cypri/OneDrive/Desktop" -type f -name '.terraform.tfstate.lock.info' -exec rm -f {} +
+# Delete terragrunt directories and files
+# find "/home/cypri/OneDrive/Desktop" -type d -name '.terragrunt' -exec rm -rf {} +
+# find "/home/cypri/OneDrive/Desktop" -type d -name '.terragrunt-cache' -exec rm -rf {} +
 echo "Cleanup complete."
